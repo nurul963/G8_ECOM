8b28d98aaf44067ab64ef5d27fc9d3597aecae7d
Md Nurul Islam <mdnuruli579@gmail.com>
2026-02-24 17:30:20 +0530

    added payment integration

--- Backend/server.js
+++ Backend/server.js
@@ -4,10 +4,17 @@ import cookieParser from "cookie-parser";
 import { PORT } from "./util/constant.js";
 import mainRoute from './routes/index.js';
 import sequelize, { connectDB } from "./config/database.js";
+import cors from 'cors';
 import './modals/index.js';
 const app=express();
 app.use(bodyParser.json());
 app.use(cookieParser());
+const corsOption={
+    origin:'*',
+    credentials:true,
+    method:['GET','POST','PUT','PATCH','DELETE','OPTION']
+}
+app.use(cors(corsOption));
 app.get("/",(req,resp)=>{
     return resp.send("Hello world");
 });
